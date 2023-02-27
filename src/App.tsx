ec16a6333ea99e793191e0fe23c65efb0a69e254
Akshay Kamble <akshay.kamble@copods.co>
2023-02-27 15:32:43 +0530

    add mock for signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
+import {
+  QueryClient,
+  QueryClientProvider
+} from 'react-query';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import "./index.scss";
 import OnboardingLayout from "./layouts/OnboardingLayout";
 import SignupLayout from "./layouts/SignupLayout";
+import { worker } from "./mocks/browser";
 import Signup from "./pages/Signup";
 //@ts-ignore
 const Onboarding = lazy(() => import('onboarding/Onboarding'));
 //@ts-ignore
 const Word = lazy(() => import('onboarding/Word'));
+const queryClient = new QueryClient()
+// To enable mocking
+if (true) {
+  worker.start()
+}
+
 const App = () => (
-  <div className="flex flex-col h-screen">
-
-
-    <Routes>
-      <Route path="/" element={<SignupLayout header={<Header />} signup={<Signup />} />} />
-      <Route path="/onboarding" element={<Suspense fallback={<h1>Loading...</h1>}><OnboardingLayout header={<Header />} onboarding={<Onboarding />} /></Suspense>} />
-      <Route path="contact" element={<h1>Contact</h1>} />
-    </Routes>
-  </div>
-
+  <QueryClientProvider client={queryClient}>
+    <div className="flex flex-col h-screen">
+      <Routes>
+        <Route path="/" element={<SignupLayout header={<Header />} signup={<Signup />} />} />
+        <Route path="/onboarding" element={<Suspense fallback={<h1>Loading...</h1>}><OnboardingLayout header={<Header />} onboarding={<Onboarding />} /></Suspense>} />
+        <Route path="contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </div>
+  </QueryClientProvider>
   // <Layout header={<Header />} />
 );
 ReactDOM.render(
--- src/components/LoginForm/LoginFrom.tsx
+++ src/components/LoginForm/LoginFrom.tsx
@@ -1,23 +1,27 @@
-import { GoogleOutlined } from "@ant-design/icons"
-import { Button } from "antd"
-import React from 'react'
-import { InputField } from "../InputField/InputField"
+import { Icon } from '@iconify/react';
+import { Button } from "antd";
+import React from 'react';
+import { InputField } from "../InputField/InputField";
 
-export const LoginForm = () => {
+
+export const LoginForm = ({ data, setEmail, email, mutate }) => {
+
     return (
         <div className='w-[442px] flex flex-col'>
             <h2 className='font-medium text-2xl mb-6'>Welcome to Stealth SaaS!</h2>
             <div className='gap-10 flex flex-col'>
-                <InputField />
-                <a className='w-full' href='/onboarding'>
-                    <Button className='bg-geekblue-600 rounded-sm w-full' type="primary">Continue</Button>
-                </a>
+                <InputField value={email} onChange={setEmail} />
+
+                <Button onClick={() => mutate(email)} className='bg-geekblue-600 rounded-sm w-full' type="primary">Continue</Button>
+
                 <div className='flex items-center gap-1 pb-4'>
                     <span className='h-[1px] bg-breaker2 w-full'></span>
                     <span>OR</span>
                     <span className='h-[1px] bg-breaker2 w-full'></span> </div>
             </div>
-            <Button icon={<GoogleOutlined />} className='border-geekblue rounded-sm flex items-center justify-center'>Continue with google</Button>
+            <div className="flex gap-4">
+                {data?.social_options?.map((option, i) => <Button key={i} icon={<Icon icon={option.icon} />} className='border-geekblue gap-2 flex-1 rounded-sm flex items-center justify-center'>Continue with {option.name}</Button>)}
+            </div>
             <p className='text-sm text-center mt-7'>Already have an account? <a className='text-geekblue-600' href="#">Log In</a></p>
         </div>
     )
